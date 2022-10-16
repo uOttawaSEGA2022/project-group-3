6ae61f6448997a002baf4c4647fdc8dc9fd5ad85
Noah do Rego <ndore051@uottawa.ca>
2022-10-16 19:50:47 -0400

    Added Classes for types of Accounts according to the UML diagram, added instance variables and getter methods in Account superclass

---
+++ app/src/main/java/com/example/mealer/Account.java
@@ -0,0 +1,14 @@
+package com.example.mealer;
+
+public class Account {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

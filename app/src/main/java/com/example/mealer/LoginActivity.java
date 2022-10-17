284f6b65d7638f1cac7082aff70e4ad2ae88a38d
Samuel Braun <samuelbraun17@gmail.com>
2022-10-16 21:11:44 -0400

    fixed double bug (real fix)

--- app/src/main/java/com/example/mealer/LoginActivity.java
+++ app/src/main/java/com/example/mealer/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
                     if (hasLogin("Admin",username.getText().toString(),password.getText().toString())){
                         returnToMain("Admin");
                     }
+
                 }
 
     }
@@ -75,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(intent, 0);
     }
 
-    private boolean hasLogin(String typeOfLogin,String username, String password){
+    private boolean hasLogin(String typeOfLogin, String username, String password){
         //checks if they are trying to login and verifies
         switch (typeOfLogin){
             case ("Cook"):
@@ -83,33 +84,30 @@ public class LoginActivity extends AppCompatActivity {
                     return true;
                 }
                 else{
-                    displayToast();
-                    break;
+                    return false;
                 }
             case ("Client"):
                 if ((username.equals("client")&&password.equals("client123"))||(RegisterPage.getClientUsername(username)&&RegisterPage.getClientPassword(password))){
                     return true;
                 }
                 else{
-                    displayToast();
-                    break;
+                    return false;
                 }
             case ("Admin"):
                 if (username.equals("admin")&&password.equals("admin123")) {
                     return true;
                 }
                 else{
-                    displayToast();
-                    break;
+                    displayToast("Wrong password or username");
+                    return false;
                 }
             default:
                 return false;
         }
-        return false;
     }
 
-    private void displayToast(){
-        Toast.makeText(LoginActivity.this, "Wrong username or password", Toast.LENGTH_SHORT).show();
+    private void displayToast(String message){
+        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
 

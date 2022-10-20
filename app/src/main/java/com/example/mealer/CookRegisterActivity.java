74938bf66a5343ca8eb567973e2f007a3e504f5f
Pradyu <pvasu009@uottawa.ca>
2022-10-20 19:57:14 -0400

    Init fields in the java for logic.

--- app/src/main/java/com/example/mealer/CookRegisterActivity.java
+++ app/src/main/java/com/example/mealer/CookRegisterActivity.java
@@ -3,12 +3,26 @@ package com.example.mealer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class CookRegisterActivity extends AppCompatActivity {
+    EditText cookFirstName, cookLastName, cookDescription, cookAddress;
+    ArrayList<String> cookFirstNames = new ArrayList<>();
+    ArrayList<String> cookLastNames = new ArrayList<>();
+    ArrayList<String> cookDescriptions = new ArrayList<>();
+    ArrayList<String> cookAddresses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_register);
+        cookFirstName=findViewById(R.id.firstNameField);
+        cookLastName=findViewById(R.id.lastNameField);
+        cookDescription=findViewById(R.id.descriptionField);
+        cookAddress=findViewById(R.id.addressField);
     }
+
+
 }

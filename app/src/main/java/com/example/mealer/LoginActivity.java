db460f2867478256cdd68755263c487c63498a89
Noah do Rego <ndore051@uottawa.ca>
2022-10-19 21:37:26 -0400

    Added methods to check for a user's existence and improved efficiency in login (n vs 2n). Also kept string arrays as temporary backup if necessary

--- app/src/main/java/com/example/mealer/LoginActivity.java
+++ app/src/main/java/com/example/mealer/LoginActivity.java
@@ -79,14 +79,14 @@ public class LoginActivity extends AppCompatActivity {
         //checks if they are trying to login and verifies
         switch (typeOfLogin){
             case ("Cook"):
-                if ((username.equals("cook")&&password.equals("cook123"))||(RegisterPage.getCookUsername(username)&&RegisterPage.getCookPassword(password))) {
+                if ((username.equals("cook")&&password.equals("cook123"))||(RegisterPage.checkCookInfo(username, password))) {
                     return true;
                 }
                 else{
                     return false;
                 }
             case ("Client"):
-                if ((username.equals("client")&&password.equals("client123"))||(RegisterPage.getClientUsername(username)&&RegisterPage.getClientPassword(password))){
+                if ((username.equals("client")&&password.equals("client123"))||(RegisterPage.checkClientInfo(username, password))){
                     return true;
                 }
                 else{

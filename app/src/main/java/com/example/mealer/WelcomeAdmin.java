381ddb0495c565ef94aa6a1ca8760df3fc0c0824
Noah do Rego <ndore051@uottawa.ca>
2022-10-19 20:00:16 -0400

    Added .java classes for Viv's welcome page templates

---
+++ app/src/main/java/com/example/mealer/WelcomeAdmin.java
@@ -0,0 +1,14 @@
+package com.example.mealer;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class WelcomeAdmin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_admin);
+    }
+}
---
+++ app/src/main/java/com/example/mealer/WelcomeClient.java
@@ -0,0 +1,13 @@
+package com.example.mealer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeClient extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_client);
+    }
+}

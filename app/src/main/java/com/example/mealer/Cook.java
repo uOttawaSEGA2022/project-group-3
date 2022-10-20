457caf931464e8553e91d5582a767d845c012308
Noah do Rego <ndore051@uottawa.ca>
2022-10-20 14:48:24 -0400

    Added extra variables to Client and Cook according to project instructions

--- app/src/main/java/com/example/mealer/Cook.java
+++ app/src/main/java/com/example/mealer/Cook.java
@@ -1,10 +1,21 @@
 package com.example.mealer;
 
+import android.media.Image;
+
 public class Cook extends Account {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String description;
+    private Image voidCheque;
 
     Cook() {
         this.username = "";
         this.password = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.address = "";
+        this.description = "";
     }
 
     Cook(String username, String password) {

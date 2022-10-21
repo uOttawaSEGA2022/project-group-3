4c8dd1c13dd6db4a1a098f05b9465cb3180aa1d0
Pradyu <pvasu009@uottawa.ca>
2022-10-20 22:10:28 -0400

    Logout button basic functionality.

--- app/src/main/java/com/example/mealer/WelcomeAdminPage.java
+++ app/src/main/java/com/example/mealer/WelcomeAdminPage.java
@@ -2,13 +2,26 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeAdminPage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_admin);
     }
+
+    public void logOutOfApp(View view){
+        sendIntentToLogin();
+    }
+
+    private void sendIntentToLogin(){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+
 }

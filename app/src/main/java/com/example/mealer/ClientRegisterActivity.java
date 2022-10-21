c91fa63e3025a40f73c4e73f53a58b1bbb370adf
Samuel Braun <samuelbraun17@gmail.com>
2022-10-20 23:10:11 -0400

    new verificators

--- app/src/main/java/com/example/mealer/ClientRegisterActivity.java
+++ app/src/main/java/com/example/mealer/ClientRegisterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ClientRegisterActivity extends AppCompatActivity {
     EditText clientFirstName, clientLastName, clientAddress, clientCreditCardNumber;
@@ -22,9 +23,86 @@ public class ClientRegisterActivity extends AppCompatActivity {
         currentAccount = (Client) getIntent().getSerializableExtra("clientObj");
     }
 
-    public boolean isFieldValid() {
-        // SAM SAM SAM SAM
+    public boolean isFieldValid(){
+
+        String firstNameInput = clientFirstName.getText().toString();
+        String lastNameInput = clientLastName.getText().toString();
+        String creditCardInput = clientCreditCardNumber.getText().toString();
+        String addressInput = clientAddress.getText().toString();
+
+        //CHECK IF A FIELD IS EMPTY
+        if ((firstNameInput.equals(""))||(lastNameInput.equals(""))||(creditCardInput.equals(""))||(addressInput.equals(""))){
+            displayToast("No empty fields");
+            return false;
+        }
+
+
+
+        //FIRST NAME CHECKS
+        if (firstNameInput.length() > 30) {
+            displayToast("First name too long.");
+            return false;
+        }
+        for(char c : firstNameInput.toCharArray()){
+            if (!isAlphaNumeric(c)) {
+                displayToast("First name contains illegal characters.");
+                return false;
+            }
+        }
+        if (Character.isWhitespace(firstNameInput.charAt(0))){
+            displayToast("First name cannot start with whitespace");
+            return false;
+        }
+
+
+
+        //LAST NAME CHECKS
+        if (lastNameInput.length() > 30) {
+            displayToast("Last name too long.");
+            return false;
+        }
+        for(char c : lastNameInput.toCharArray()){
+            if (!isAlphaNumeric(c)) {
+                displayToast("Last name contains illegal characters.");
+                return false;
+            }
+        }
+        if (Character.isWhitespace(lastNameInput.charAt(0))){
+            displayToast("Last name cannot start with whitespace");
+            return false;
+        }
+
+
+
+        //CREDIT CARD CHECKS
+        if (creditCardInput.length() != 16) {
+            displayToast("Credit card number must be 16 digits.");
+            return false;
+        }
+
+        //ADDRESS CHECKS
+        if (addressInput.length() > 100) {
+            displayToast("Address too long.");
+            return false;
+        }
+        if (Character.isWhitespace(addressInput.charAt(0))){
+            displayToast("Address cannot start with whitespace");
+            return false;
+        }
+
         return true;
+    }
+
+    public static boolean isAlphaNumeric(char char1) {
+        return (char1 >= 'a' && char1 <= 'z') || (char1 >= 'A' && char1 <= 'Z') || (char1 == ' ');
+    }
+
+    public static boolean isNumeric(char char1) {
+        return (char1 >= '1' && char1 <= 'z') || (char1 >= 'A' && char1 <= 'Z') || (char1 == ' ');
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(ClientRegisterActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
     public void registerAsCook(View view){

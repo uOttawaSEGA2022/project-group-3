e45ff6f97bd7e1f61ad1dc2332707ff1031b1bb0
Maria <113953469+MariaKhalil999@users.noreply.github.com>
2022-11-02 23:04:39 -0400

    added inbox activity (#8)
    LGTM Maria! Keep it up!

---
+++ app/src/main/java/com/example/mealer/AdminInboxActivity.java
@@ -0,0 +1,17 @@
+package com.example.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class AdminInboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_inbox);
+    }
+
+
+}

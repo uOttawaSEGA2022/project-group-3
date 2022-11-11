2c9c5d62a12a109f8c31140767597380117a4dbd
Pradyu <pvasu009@uottawa.ca>
2022-11-11 14:50:12 -0500

    Added cook menu activities.

---
+++ app/src/main/java/com/example/mealer/CookPersonalMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookPersonalMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_personal_menu);
+    }
+}

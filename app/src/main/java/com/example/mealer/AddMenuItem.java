62df4f1430bdee22480a48fd984c9e9320884123
Pradyu <pvasu009@uottawa.ca>
2022-11-11 19:44:47 -0500

    Added activity for adding a menu item, created object that will deal with that.

--- app/src/main/java/com/example/mealer/AddMenuItem.java
+++ app/src/main/java/com/example/mealer/AddMenuItem.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CookPersonalMenuActivity extends AppCompatActivity {
+public class AddMenuItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cook_personal_menu);
+        setContentView(R.layout.activity_add_menu_item);
     }
-
-
 }
---
+++ app/src/main/java/com/example/mealer/MenuItem.java
@@ -0,0 +1,33 @@
+package com.example.mealer;
+
+public class MenuItem {
+    private String title;
+    private String description;
+    private String ingredients;
+    private String cookID;
+
+    public MenuItem(String t, String d, String i,String c){
+        this.title=t;
+        this.description=d;
+        this.ingredients=i;
+        this.cookID=c;
+    }
+
+    public void setTitle(String t){ this.title=t; }
+
+    public void setDescription (String d){ this.description=d; }
+
+    public void setIngredients (String i){ this.ingredients=i; }
+
+    public void setCookID(String c){ this.cookID=c; }
+
+    public String getTitle(){ return title; }
+
+    public String getDescription() { return description; }
+
+    public String getIngredients() { return ingredients; }
+
+    public String getCookID(){ return cookID; }
+
+
+}

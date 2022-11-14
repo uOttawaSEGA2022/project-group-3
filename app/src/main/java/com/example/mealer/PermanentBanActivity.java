dcda8d6925d110d306d015f652f8d02c4933db2d
Vivethen Balachandiran <vbala087@uottawa.ca>
2022-11-14 14:05:41 -0500

    Added log off buttons to both ban pages

--- app/src/main/java/com/example/mealer/PermanentBanActivity.java
+++ app/src/main/java/com/example/mealer/PermanentBanActivity.java
@@ -2,7 +2,11 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PermanentBanActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class PermanentBanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permanent_ban);
+    }
+
+    public void logOutOfApp(View view){
+        FirebaseAuth.getInstance().signOut();
+        sendIntentToLogin();
+    }
+
+    private void sendIntentToLogin(){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(intent, 0);
     }
 }

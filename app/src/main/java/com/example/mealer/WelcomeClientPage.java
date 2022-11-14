60d19f43defe2e40c38e110ca718662a109afe6b
Noah do Rego <ndore051@uottawa.ca>
2022-11-14 15:06:41 -0500

    added comments in all welcome pages

--- app/src/main/java/com/example/mealer/WelcomeClientPage.java
+++ app/src/main/java/com/example/mealer/WelcomeClientPage.java
@@ -19,21 +19,29 @@ import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeClientPage extends AppCompatActivity {
 
+    // Instance variables
+
     FirebaseUser user;
     DatabaseReference database;
     private String userID;
 
+    /*
+     * OnCreate method
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_client);
 
+        // variables for storing firebase info
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance().getReference("accounts");
         userID = user.getUid();
 
+        // get welcome text view
         TextView welcomeTextView = (TextView) findViewById(R.id.welcomeMessage);
 
+        // adding a listener to the database object with current user id to get values
         database.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -55,16 +63,19 @@ public class WelcomeClientPage extends AppCompatActivity {
         });
     }
 
+    // method for logging out
     public void logOutOfApp(View view){
         FirebaseAuth.getInstance().signOut();
         sendIntentToLogin();
     }
 
+    // send user to login
     private void sendIntentToLogin(){
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivityForResult(intent, 0);
     }
 
+    // method for displaying toast
     public void displayToast(String message){
         Toast.makeText(WelcomeClientPage.this, message, Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/com/example/mealer/WelcomeCookPage.java
+++ app/src/main/java/com/example/mealer/WelcomeCookPage.java
@@ -26,27 +26,36 @@ import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeCookPage extends AppCompatActivity {
 
+    // Instance variables
     FirebaseUser user;
     DatabaseReference database;
     private String userID;
     private static Context viewPage;
     TextView welcomeText;
 
-
+    /*
+     * OnCreate method
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_cook);
 
         viewPage = this;
+
+        // get welcome text view
         welcomeText=findViewById(R.id.welcomeMessage);
+
+        // variables for storing firebase info
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance().getReference("accounts");
         userID = user.getUid();
 
+        // adding a listener to the database object with current user id to get values
         database.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // check if cook is permanently banned
                 String permBan = snapshot.child("permanentBan").getValue().toString();
                 if (permBan.equals("true")){
                     setContentView(R.layout.activity_permanent_ban);
@@ -62,6 +71,7 @@ public class WelcomeCookPage extends AppCompatActivity {
         database.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // check for temporary ban
                 String tempBan = snapshot.child("temporaryBan").getValue().toString();
 
                 if (!tempBan.equals("null")){
@@ -126,20 +136,24 @@ public class WelcomeCookPage extends AppCompatActivity {
         });
     }
 
+    // method for logging
     public void logOutOfApp(View view){
         FirebaseAuth.getInstance().signOut();
         sendIntentToLogin();
     }
 
+    // send user to login
     private void sendIntentToLogin(){
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivityForResult(intent, 0);
     }
 
+    // method for displaying toast
     public void displayToast(String message){
         Toast.makeText(WelcomeCookPage.this, message, Toast.LENGTH_SHORT).show();
     }
 
+    // method for sending to current cook's personal menu
     public void sendToPersonalMenu(View view){
         Intent intent = new Intent(getApplicationContext(), CookPersonalMenuActivity.class);
         startActivityForResult(intent,0);
